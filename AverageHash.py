056cfc02079b1ce8dcb3e696df244d233ce54945
Yunpeng Chen <37839207+Yunpneg@users.noreply.github.com>
2018-08-28 15:26:31 +0100

    3.1.4 AverageHash(Intra-class and Inter-class)

--- AverageHash.py
+++ AverageHash.py
@@ -1,12 +1,8 @@
-import pytesseract
-from pytesser3 import *
-from PIL import Image, ImageEnhance, ImageFilter
+from keras.preprocessing.image import load_img, img_to_array
+from PIL import Image
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 
-# import hashlib
 
 def getGray(image_file):
     tmpls = []
@@ -17,11 +13,11 @@ def getGray(image_file):
     return tmpls
 
 
-def getAvg(ls):  # Get the average grayscale value
+def getAvg(ls):
     return sum(ls) / len(ls)
 
 
-def getMH(a, b):  # Compare the 100 characters and find the number of same characters between two strings
+def getMH(a, b):  
     dist = 0
     for i in range(0, len(a)):
         if a[i] == b[i]:
@@ -32,31 +28,37 @@ def getMH(a, b):  # Compare the 100 characters and find the number of same chara
 def getImgHash(fne):
     image_file = Image.open(fne)
     image_file = image_file.resize((8, 8))
-    image_file = image_file.convert("L")  # Convert to grayscale image
+    image_file = image_file.convert("L")
     Grayls = getGray(image_file)
-
-    avg = getAvg(Grayls)  # Average value of grayscale
+    avg = getAvg(Grayls)
     bitls = ''
     for h in range(1, image_file.size[1] - 1):  # height
         for w in range(1, image_file.size[0] - 1):  # width
             if image_file.getpixel((w, h)) >= avg:
-                # compare with the average value
-                # if it is greater than the average value, recoded as 1; otherwise, recorded as 0
                 bitls = bitls + '1'
             else:
                 bitls = bitls + '0'
     return bitls
 
-
+# file path we want to test
 dirname_path = 'test_data'
-true_label = []
+# Get total number of images
+count = 0
+for root, dirs, files in os.walk(dirname_path):
+    for each in files:
+        if each != '.DS_Store':
+            count += 1
+images_array = np.zeros((count, 224, 224, 3))
+
+t = 0
 path_list = []
-file_list = []
+true_label = [] # Ground Truth list
+name_list = []  # file name list
 dir_path_list = os.listdir(dirname_path)
 if '.DS_Store' in dir_path_list:
     dir_path_list.remove('.DS_Store')
 dir_path_list.sort()
-class_item = 0
+class_item = 0  # index of category
 for dirname in dir_path_list:
     file_path = dirname_path + '/' + dirname
     file_path_list = os.listdir(file_path)
@@ -65,13 +67,19 @@ for dirname in dir_path_list:
     file_path_list.sort()
     for filename in file_path_list:
         img_path = file_path + '/' + filename
-        file_list.append(filename)
+        name_list.append(filename)
         true_label.append(class_item)
         path_list.append(img_path)
-    class_item += 1
+        # load the images
+        image = load_img(img_path, target_size=(224, 224, 3))
+        image = img_to_array(image)  # shape is (224,224,3)
+        images_array[t] = image  # (224, 224, 3). float64
+        t += 1
 
+    class_item += 1  # category number
+# create 9 lists to contain the index of normal flower and the index of eight types of mutant flower.
 normal_index = []
-mutation_index =[]
+image_index =[]
 mutation_index_1 =[]
 mutation_index_2 =[]
 mutation_index_3 =[]
@@ -91,53 +99,55 @@ mutation_index_5.append([t for t, x in enumerate(true_label) if x == 5])
 # mutation_index_7.append([t for t, x in enumerate(true_label) if x == 7])
 # mutation_index_8.append([t for t, x in enumerate(true_label) if x == 8])
 
-mutation_index.append(mutation_index_1)
-mutation_index.append(mutation_index_2)
-mutation_index.append(mutation_index_3)
-mutation_index.append(mutation_index_4)
-mutation_index.append(mutation_index_5)
+image_index.append(normal_index)
+image_index.append(mutation_index_1)
+image_index.append(mutation_index_2)
+image_index.append(mutation_index_3)
+image_index.append(mutation_index_4)
+image_index.append(mutation_index_5)
 # mutation_index.append(mutation_index_6)
 # mutation_index.append(mutation_index_7)
 # mutation_index.append(mutation_index_8)
 
-# The difference between mutant flowers and normal flowers
-whole_same_list = []
-for t in range(len(mutation_index)):
-    mutation_file_list = []
-    same_list = []
-    for i in mutation_index[t][0]:
-        same = 0
-        num = 0
-        a = getImgHash(path_list[i])
-        mutation_file_list.append(file_list[i])
-        for j in normal_index[0]:
-            b = getImgHash(path_list[j])
-            same += getMH(a, b)
-            num += 1
+# Average Hash
+# The average distance of intra-class
+intra_distance = []
+for i in range(len(image_index)):
+    single_sample = []
+    for j in image_index[i][0]:
+        num_1 = 0
+        ave_distance = 0
+        a = getImgHash(path_list[j])
+        for k in image_index[i][0]:
+            if j != k:
+                num_1 += 1
+                b = getImgHash(path_list[k])
+                ave_distance += getMH(a, b)
+        single_sample.append(ave_distance / num_1)
+    intra_distance.append(int(sum(single_sample)) / len(single_sample))
+intra_ave_feature = int(sum(intra_distance)) / len(intra_distance)
+print("Intra_class average distance for Average Hash", ("%.2f" % intra_ave_feature))
 
-        same_list.append(float("%.2f" % (same / num)))
-
-    whole_same_list.append(same_list)
-    print("file name", mutation_file_list)
-
-print("Difference between mutation and normal flower: ", whole_same_list)
-
-
-# Baseline
-base_list = []
-for i in normal_index[0]:
-    a = getImgHash(path_list[i])
-    base = 0
-    num_ = 0
-    for j in normal_index[0]:
-        if i != j:
-            b = getImgHash(path_list[j])
-            base += getMH(a, b)
-            num_ += 1
-    base_list.append(float(base/num_))
-ave_base = "%.2f" % (int(sum(base_list))/len(base_list))
-print("baseline", ave_base)
-
+# The average distance of Inter-class
+inter_distance = []
+for i in range(len(image_index)):
+    single_sample = []
+    for j in image_index[i][0]:
+        num_2 = 0
+        ave_distance = 0
+        index = 0
+        a = getImgHash(path_list[j])
+        while index < len(image_index):
+            if index != i:
+                for k in image_index[index][0]:
+                    num_2 += 1
+                    b = getImgHash(path_list[k])
+                    ave_distance += getMH(a, b)
+            index += 1
+        single_sample.append(ave_distance / num_2)
+    inter_distance.append(int(sum(single_sample)) / len(single_sample))
+inter_ave_feature = int(sum(inter_distance)) / len(inter_distance)
+print("Inter_class average distance for Average Hash", ("%.2f" % inter_ave_feature))
 
 
 
